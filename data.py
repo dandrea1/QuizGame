33ab482c28a1e5300b8fea2f707aec6c6cbfc470
dandrea1 <47754826+dandrea1@users.noreply.github.com>
2020-12-21 07:52:13 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,35 @@
+# noinspection SpellCheckingInspection
+question_data = [
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Gumbo is a stew that originated in Louisiana.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "French is an official language in Canada.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "It is automatically considered entrapment in the United States if the "
+                 "police sell you illegal substances without revealing themselves.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "The Sun rises from the North.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "In 2010, Twitter and the United States Library of Congress partnered together to"
+                 " archive every tweet by American citizens.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "The Lego Group was founded in 1932.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Scotland voted to become an independent country during the referendum from September 2014.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "The National Animal of Scotland is the Unicorn.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Slovakia is a member of European Union-", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "The mitochondria is the powerhouse of the cell.",
+     "correct_answer": "True", "incorrect_answers": ["False"]}
+]
